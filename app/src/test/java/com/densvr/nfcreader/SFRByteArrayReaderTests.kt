ca35d8779cbf7793d74cc2bf4a2f3168fede087f
i-sergeev <master.hensh@gmail.com>
2020-04-07 09:46:39 +0300

    Add tests for int16 parser

--- app/src/test/java/com/densvr/nfcreader/SFRByteArrayReaderTests.kt
+++ app/src/test/java/com/densvr/nfcreader/SFRByteArrayReaderTests.kt
@@ -10,7 +10,7 @@ import kotlin.test.assertFails
 class SFRByteArrayReaderTests {
 
     @Test
-    fun readZeroInt16FromBytesAtStartPositionTest() {
+    fun readZeroInt16FromBytesAtZeroPositionTest() {
         val bytes = byteArrayOf(0, 0)
         assertEquals(0, bytes.readInt16Value(0))
     }
@@ -31,5 +31,29 @@ class SFRByteArrayReaderTests {
     fun failReadInt16FromBytesAtNegativePositionTest() {
         val bytes = byteArrayOf(0)
         assertFails("Byte array is to small for read int16") { bytes.readInt16Value(-1) }
+    }
+
+    @Test
+    fun readSimpleInt16FromBytesAtZeroPositionTest() {
+        assertEquals(1, byteArrayOf(0, 1).readInt16Value(0))
+        assertEquals(15, byteArrayOf(0, 15).readInt16Value(0))
+        assertEquals(127, byteArrayOf(0, 127).readInt16Value(0))
+        assertEquals(255, byteArrayOf(0, -1).readInt16Value(0))
+        assertEquals(256, byteArrayOf(1, 0).readInt16Value(0))
+        assertEquals(32512, byteArrayOf(127, 0).readInt16Value(0))
+        assertEquals(32768, byteArrayOf(-128, 0).readInt16Value(0))
+        assertEquals(16188, byteArrayOf(63, 60).readInt16Value(0))
+    }
+
+    @Test
+    fun readSimpleInt16FromBytesAtNotZeroPositionTest() {
+        assertEquals(1, byteArrayOf(0, 0, 0, 0, 1, 0, 0, 0).readInt16Value(3))
+        assertEquals(15, byteArrayOf(0, 0, 0, 15, 0, 0, 0).readInt16Value(2))
+        assertEquals(127, byteArrayOf(0, 0, 127).readInt16Value(1))
+        assertEquals(255, byteArrayOf(0, 0, 0, 0, 0, -1).readInt16Value(4))
+        assertEquals(256, byteArrayOf(0, 0, 1, 0).readInt16Value(2))
+        assertEquals(32512, byteArrayOf(0, 0, 0, 0, 0, 0, 127, 0, 0, 0).readInt16Value(6))
+        assertEquals(32768, byteArrayOf(0, 0, 0, 0, 0, -128, 0, 0, 0).readInt16Value(5))
+        assertEquals(16188, byteArrayOf(0, 63, 60).readInt16Value(1))
     }
 }
