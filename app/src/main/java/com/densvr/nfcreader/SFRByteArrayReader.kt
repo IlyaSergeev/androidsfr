00052b000fe767bc2fc5a18f9d9d03e6965ac41a
i-sergeev <master.hensh@gmail.com>
2020-04-07 09:47:38 +0300

    Fix bugs with parser int16 values

--- app/src/main/java/com/densvr/nfcreader/SFRByteArrayReader.kt
+++ app/src/main/java/com/densvr/nfcreader/SFRByteArrayReader.kt
@@ -11,12 +11,22 @@ private fun Int.asSFRChipType(): SFRChipType? {
     }
 }
 
-internal fun ByteArray.readInt16Value(position: Int): Int {
-    return (this[position].toInt() shl 8) + this[position + 1]
+private fun ByteArray.readIntValue(position: Int, intLength: Int): Int {
+    var result = 0
+    for (i in position until (position + intLength)) {
+        result = (result shl 8) or (0xFF and this[i].toInt())
+    }
+    return result
 }
 
+private const val INT_16_SIZE_BYTES = 2
+internal fun ByteArray.readInt16Value(position: Int): Int {
+    return readIntValue(position, INT_16_SIZE_BYTES)
+}
+
+private const val INT_32_SIZE_BYTES = 2
 internal fun ByteArray.readInt32Value(position: Int): Int {
-    return (this[position].toInt() shl 24) + (this[position + 1].toInt() shl 16) + (this[position + 2].toInt() shl 8) + this[position + 3]
+    return readIntValue(position, INT_32_SIZE_BYTES)
 }
 
 internal fun ByteArray.readSFROperationInfo(position: Int): SFROperationInfo {
@@ -28,22 +38,22 @@ internal fun ByteArray.readSFROperationInfo(position: Int): SFROperationInfo {
 
 internal fun ByteArray.readChipNumber(position: Int): Int {
     return readInt16Value(position) +
-            200 * readInt16Value(position + 2) +
-            40000 * readInt16Value(position + 4)
+            200 * readInt16Value(position + INT_16_SIZE_BYTES) +
+            40000 * readInt16Value(position + 2 * INT_16_SIZE_BYTES)
 }
 
 internal fun ByteArray.readSFRRecordInfo(position: Int): SFRRecordInfo {
     return SFRRecordInfo(
         readInt16Value(position),
-        readSFRTime(position + 2)
+        readSFRTime(position + INT_16_SIZE_BYTES)
     )
 }
 
 internal fun ByteArray.readSFRTime(position: Int): Long {
     return createDelayMillis(
-        hours = readIntFromDecimalFormat(position, 2),
-        minutes = readIntFromDecimalFormat(position + 2, 2),
-        seconds = readIntFromDecimalFormat(position + 4, 2)
+        hours = readIntFromDecimalFormat(position, INT_16_SIZE_BYTES),
+        minutes = readIntFromDecimalFormat(position + INT_16_SIZE_BYTES, INT_16_SIZE_BYTES),
+        seconds = readIntFromDecimalFormat(position + 2 * INT_16_SIZE_BYTES, INT_16_SIZE_BYTES)
     )
 }
 
