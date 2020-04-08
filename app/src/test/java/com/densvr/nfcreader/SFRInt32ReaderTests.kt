8b79183028e65ba700642cc7ffc09ff402611872
iSergeev <master.hensh@gmail.com>
2020-04-08 09:28:46 +0300

    Add tests for int32 parser

--- app/src/test/java/com/densvr/nfcreader/SFRInt32ReaderTests.kt
+++ app/src/test/java/com/densvr/nfcreader/SFRInt32ReaderTests.kt
@@ -56,4 +56,28 @@ class SFRInt32ReaderTests {
         assertEquals(32768, byteArrayOf(0, 0, 0, 0, 0, 0, 0, -128, 0, 0, 0).readInt32Value(5))
         assertEquals(16188, byteArrayOf(0, 0, 0, 63, 60).readInt32Value(1))
     }
+
+    @Test
+    fun readLargeInt32FromBytesAtZeroPositionTest() {
+        assertEquals(16908289, byteArrayOf(1, 2, 0, 1).readInt32Value(0))
+        assertEquals(16908303, byteArrayOf(1, 2, 0, 15).readInt32Value(0))
+        assertEquals(2013462655, byteArrayOf(120, 3, 0, 127).readInt32Value(0))
+        assertEquals(917759, byteArrayOf(0, 14, 0, -1).readInt32Value(0))
+        assertEquals(1191182592, byteArrayOf(71, 0, 1, 0).readInt32Value(0))
+        assertEquals(303202048, byteArrayOf(18, 18, 127, 0).readInt32Value(0))
+        assertEquals(239239168, byteArrayOf(14, 66, -128, 0).readInt32Value(0))
+        assertEquals(1845509948, byteArrayOf(110, 0, 63, 60).readInt32Value(0))
+    }
+
+    @Test
+    fun readLargeInt32FromBytesAtNotZeroPositionTest() {
+        assertEquals(16908289, byteArrayOf(0, 0, 1, 2, 0, 1).readInt32Value(2))
+        assertEquals(16908303, byteArrayOf(0, 0, 0, 1, 2, 0, 15).readInt32Value(3))
+        assertEquals(2013462655, byteArrayOf(0, 0, 0, 0, 0, 120, 3, 0, 127).readInt32Value(5))
+        assertEquals(917759, byteArrayOf(0, 0, 14, 0, -1).readInt32Value(1))
+        assertEquals(1191182592, byteArrayOf(0, 71, 0, 1, 0).readInt32Value(1))
+        assertEquals(303202048, byteArrayOf(0, 0, 18, 18, 127, 0).readInt32Value(2))
+        assertEquals(239239168, byteArrayOf(0, 0, 0, 0, 14, 66, -128, 0).readInt32Value(4))
+        assertEquals(1845509948, byteArrayOf(0, 0, 0, 0, 0, 0, 110, 0, 63, 60).readInt32Value(6))
+    }
 }
