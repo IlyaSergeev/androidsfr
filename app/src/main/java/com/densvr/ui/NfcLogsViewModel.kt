8a318202ed2bc2aa12ac7eaa69a7913e40ce1955
iSergeev <master.hensh@gmail.com>
2020-04-15 17:04:31 +0300

    Highlight error on it happened

--- app/src/main/java/com/densvr/ui/NfcLogsViewModel.kt
+++ app/src/main/java/com/densvr/ui/NfcLogsViewModel.kt
@@ -1,24 +1,39 @@
 package com.densvr.ui
 
+import android.graphics.Color
+import android.text.SpannableStringBuilder
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.densvr.nfcreader.asNumeratedString
+import com.densvr.util.withColor
 import java.text.SimpleDateFormat
 import java.util.*
 
 class NfcLogsViewModel : ViewModel() {
 
-    private val mutableLastReadLogs = MutableLiveData<String>().apply {
+    private val mutableLastReadLogs = MutableLiveData<CharSequence>().apply {
         value = ""
     }
 
-    val lastReadLogs: LiveData<String> = mutableLastReadLogs
+    val lastReadLogs: LiveData<CharSequence> = mutableLastReadLogs
 
     fun setLogs(bytes: ByteArray, error: Throwable?, operationDate: Date) {
-        mutableLastReadLogs.value =
-            SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.MEDIUM)
-                .format(operationDate) + "\n\n" + bytes.asNumeratedString() +
-                    if (error != null) "\n\n!!!  ${error.message}" else ""
+        val stringBuilder = SpannableStringBuilder()
+            .append(
+                SimpleDateFormat.getDateTimeInstance(
+                    SimpleDateFormat.SHORT,
+                    SimpleDateFormat.MEDIUM
+                ).format(operationDate)
+            )
+            .append("\n\n")
+            .append(bytes.asNumeratedString())
+
+        error?.let { storedError ->
+            stringBuilder.append("\n\n")
+                .append("!!!  ${error.message}".withColor(Color.RED))
+        }
+
+        mutableLastReadLogs.value = stringBuilder
     }
 }
---
+++ app/src/main/java/com/densvr/util/StringColorExtentions.kt
@@ -0,0 +1,11 @@
+package com.densvr.util
+
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+
+fun String.withColor(color: Int): SpannableString {
+    return SpannableString(this).apply {
+        setSpan(ForegroundColorSpan(color), 0, length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+}
