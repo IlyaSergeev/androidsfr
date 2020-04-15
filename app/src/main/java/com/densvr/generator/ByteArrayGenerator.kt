ddd8353eeed1ba493894a53830891d8717e9e6d4
iSergeev <master.hensh@gmail.com>
2020-04-15 16:14:37 +0300

    fix crash on generate empty nfc record

--- app/src/main/java/com/densvr/generator/ByteArrayGenerator.kt
+++ app/src/main/java/com/densvr/generator/ByteArrayGenerator.kt
@@ -6,6 +6,10 @@ import kotlin.random.Random
 
 fun Random.nextSfrRecordBytes(): ByteArray {
     val recordsCount = nextInt(122)
-    val bytesCount = nextInt(SFR_HEAD_SIZE_BYTES, SFR_HEAD_SIZE_BYTES + recordsCount)
+    val bytesCount = if (recordsCount > 0) {
+        nextInt(SFR_HEAD_SIZE_BYTES, SFR_HEAD_SIZE_BYTES + recordsCount)
+    } else {
+        SFR_HEAD_SIZE_BYTES
+    }
     return nextBytes(bytesCount * SRF_BLOCK_SIZE_BYTES)
 }
