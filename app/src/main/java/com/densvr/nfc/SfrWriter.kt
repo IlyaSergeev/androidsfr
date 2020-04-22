88f8622dbf9d732d6b9884014e91b71cd1da4984
i-sergeev <master.hensh@gmail.com>
2020-04-22 18:12:52 +0300

    Fix nfcWriter logic

--- app/src/main/java/com/densvr/nfc/SfrWriter.kt
+++ app/src/main/java/com/densvr/nfc/SfrWriter.kt
@@ -2,15 +2,13 @@ package com.densvr.nfc
 
 import android.nfc.tech.NfcV
 import com.densvr.util.retryOnError
-import timber.log.Timber
-import kotlin.experimental.xor
 
 private const val NFC_WRITE_TRY_COUNTS = 3
 
-internal const val NFC_WRITE_BLOCK_FLAG = 0x47.toByte()
+internal const val NFC_WRITE_BLOCK_FLAG = 0x42.toByte()
 
 private const val NFC_WRITE_TAG_INDEX_POSITION = 2
-private const val NFC_WRITE_TAG_BLOCK_START_POSITION = 3
+private const val NFC_WRITE_TAG_BLOCK_START_POSITION = 2
 
 private val writeSfrTagCommand =
     arrayListOf<Byte>().apply {
@@ -26,50 +24,18 @@ private val writeSfrTagCommand =
 
 fun NfcV.writeTag(tagIndex: Byte, bytes: ByteArray) {
 
-//    var flag = 0x40.toByte()
-//    while ((flag.uint xor 0xFF) > 0) {
-//        try {
-//            retryOnError(
-//                NFC_WRITE_TRY_COUNTS,
-//                { throwable -> true },
-//                {
-//                    writeSfrTagCommand[0] = flag
-//                    writeSfrTagCommand[NFC_WRITE_TAG_INDEX_POSITION] = tagIndex
-//                    bytes.copyInto(
-//                        writeSfrTagCommand,
-//                        NFC_WRITE_TAG_BLOCK_START_POSITION,
-//                        0,
-//                        SRF_BLOCK_SIZE_BYTES
-//                    )
-//
-//                    transceive(writeSfrTagCommand).also {
-//                        Timber.tag("NFC").d("success $flag")
-//                        it.logAsNfcWriteMessage(tagIndex, writeSfrTagCommand, "Write single tag ")
-//                    }
-//                        .parseNfcMessage(
-//                            { bytes, offset -> },
-//                            { nfcResponseCode -> }
-//                        )
-//                }
-//            )
-//        }
-//        catch (error: Throwable) {
-//            Timber.tag("NFC").d("error $flag")
-//        }
-//        flag = (flag + 1).toByte()
-//    }
+    writeSfrTagCommand[NFC_WRITE_TAG_INDEX_POSITION] = tagIndex
+    bytes.copyInto(
+        writeSfrTagCommand,
+        NFC_WRITE_TAG_BLOCK_START_POSITION,
+        0,
+        SRF_BLOCK_SIZE_BYTES
+    )
+
     return retryOnError(
         NFC_WRITE_TRY_COUNTS,
         { throwable -> true },
         {
-            writeSfrTagCommand[NFC_WRITE_TAG_INDEX_POSITION] = tagIndex
-            bytes.copyInto(
-                writeSfrTagCommand,
-                NFC_WRITE_TAG_BLOCK_START_POSITION,
-                0,
-                SRF_BLOCK_SIZE_BYTES
-            )
-
             transceive(writeSfrTagCommand).also {
                 it.logAsNfcWriteMessage(tagIndex, writeSfrTagCommand, "Write single tag ")
             }
